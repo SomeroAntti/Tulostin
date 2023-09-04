7468264bfbd75bf68431a207fcf41eef66609dc8
Antti Somero <prima.antti.somero@gmail.com>
2023-09-04 13:55:10 +0300

    Alustettu tekijä, jonka nimen ohjelma tulostaa.

--- src/Tulostusohjelma.java
+++ src/Tulostusohjelma.java
@@ -3,6 +3,11 @@ public class Tulostusohjelma {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		String tekija = "Antti";
+		
+		System.out.println("Hei olen Tulostin-ohjelma");
+		System.out.println("Ohjelman tekijä: " + tekija);
 
 	}
 
